24cef4fc5b40c45df91dcca32ea930bc3bed9e1c
unknown <a11rasbj@SD30504.hs.local>
2013-01-25 11:25:15 +0100

    Rasmus Uppladdning
    Animationer och playerKlasser

---
+++ RobotSplit/RobotSplit/PlayerPartBody.h
@@ -0,0 +1,40 @@
+#ifndef INC_PLAYERPARTBODY
+#define INC_PLAYERPARTBODY
+#include "PlayerPart.h"
+#include "Animation.h"
+#include "PlayerPartFeet.h"
+
+class PlayerPartBody:public PlayerPart
+{
+public:
+	PlayerPartBody(PlayerPart* Feet);
+	virtual void update(); 
+	virtual void draw(); 
+	virtual sf::Vector2f getPosition(); 
+	virtual sf::Sprite getSprite(); 
+	virtual std::string getId();
+	virtual void setPosition(sf::Vector2f);
+	virtual void restartTimer();
+	virtual bool getAttached();
+	virtual void setAttached(bool);
+	virtual void jump();
+	virtual void resetAnimation();
+	virtual GameObject* getObject();
+private: 
+	bool mAttached;
+	GameObject* mObject;
+	PlayerPartFeet Test;
+	Animation* mActiveAnimation;
+	Animation mLeftAnimation;
+	Animation mRightAnimation;
+	Animation mLeft;
+	Animation mRight;
+	PlayerPart* mFeet;
+	sf::Vector2f mPosition; 
+	std::string mId;
+	float mJump, mAniTime;
+	sf::Clock mJumpClock, mAnimationTimer;
+};
+
+
+#endif
