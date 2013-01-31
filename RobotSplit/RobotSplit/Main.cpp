62675d8af6d172ec884c59bc7deef981b6ff9616
Robbin Torstensson <rotor00a@hotmail.com>
2013-01-31 14:09:27 +0100

    Laggfix

--- RobotSplit/RobotSplit/Main.cpp
+++ RobotSplit/RobotSplit/Main.cpp
@@ -42,69 +42,77 @@ int main()
 
 	Player* mPlayer= new Player(level.getPlayer()->getCollisionSprite()[0]->getPosition());
 
+	sf::Clock lastUpdateClock;
+	double lastUpdate=0;
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		lastUpdateClock.restart();
+		while (lastUpdate>1/60.0)
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-			if(event.type == sf::Event::KeyPressed){
-				switch(event.key.code)
-				{
-				case sf::Keyboard::LShift:
-					mPlayer->interact(6);
-					//mPlayer->setBodyActive(!mPlayer->getBodyActive());
-					break;
-				case sf::Keyboard::Space:
-					mPlayer->interact(5);
-					//mPlayer->setTogether(!mPlayer->getTogether());
-					break;
-				case sf::Keyboard::E:
-					mPlayer->interact(7);
-					/*if(mPlayer->getTogether()==false && mPlayer->getBodyActive()==false){
-						mPlayer->setAttachFeetExtension(!mPlayer->getAttachFeetExtension());
-					}*/
-					break;
+			lastUpdate-=1/60.0;
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+				if(event.type == sf::Event::KeyPressed){
+					switch(event.key.code)
+					{
+					case sf::Keyboard::LShift:
+						mPlayer->interact(6);
+						//mPlayer->setBodyActive(!mPlayer->getBodyActive());
+						break;
+					case sf::Keyboard::Space:
+						mPlayer->interact(5);
+						//mPlayer->setTogether(!mPlayer->getTogether());
+						break;
+					case sf::Keyboard::E:
+						mPlayer->interact(7);
+						/*if(mPlayer->getTogether()==false && mPlayer->getBodyActive()==false){
+							mPlayer->setAttachFeetExtension(!mPlayer->getAttachFeetExtension());
+						}*/
+						break;
+					}
 				}
 			}
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::R)){
+				mPlayer->reFuel(100);
+			}
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+				mPlayer->interact(3);
+			}
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+				mPlayer->interact(1);
+			}
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+				mPlayer->interact(2);
+			}
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+				mPlayer->interact(0);
+			}
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)){
+				mPlayer->interact(4);
+			}
+			if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && sf::Event::MouseButtonPressed){
+				sf::Vector2f Temp;
+				Temp.x=(float)sf::Mouse::getPosition(window).x-16;
+				Temp.y=(float)sf::Mouse::getPosition(window).y-16;
+				mPlayer->shootHead(sf::Vector2f(Temp));
+				//std::cout << "Anropar";
+			}
+			/*if(sf::Mouse::isButtonPressed(sf::Mouse::Right)){
+				mPlayer->shootHead(sf::Vector2f(0, 0));
+			}*/
+			window.clear(sf::Color::Black);
+			
+			mPlayer->update();
+
+			runCollisions(Objects, *mPlayer);
 		}
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::R)){
-			mPlayer->reFuel(100);
-		}
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-			mPlayer->interact(3);
-		}
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
-			mPlayer->interact(1);
-		}
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-			mPlayer->interact(2);
-		}
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
-			mPlayer->interact(0);
-		}
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)){
-			mPlayer->interact(4);
-		}
-		if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && sf::Event::MouseButtonPressed){
-			sf::Vector2f Temp;
-			Temp.x=(float)sf::Mouse::getPosition(window).x-16;
-			Temp.y=(float)sf::Mouse::getPosition(window).y-16;
-			mPlayer->shootHead(sf::Vector2f(Temp));
-			//std::cout << "Anropar";
-		}
-		/*if(sf::Mouse::isButtonPressed(sf::Mouse::Right)){
-			mPlayer->shootHead(sf::Vector2f(0, 0));
-		}*/
-		window.clear(sf::Color::Black);
-		
+
 		window.draw(BG->draw());
 		BG->update();
-		mPlayer->update();
 
-		runCollisions(Objects, *mPlayer);
-		
 		mPlayer->draw(window);
 		mPlayer->resetAnimations();
 
@@ -114,6 +122,8 @@ int main()
 			Objects[i]->draw();
 		}
 		window.display();
+
+		lastUpdate+=lastUpdateClock.getElapsedTime().asSeconds();
 	}
 	/*
 	//Test for finding Textures' names
