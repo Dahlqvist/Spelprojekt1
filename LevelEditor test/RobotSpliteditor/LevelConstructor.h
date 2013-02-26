54917e91da542d1f57a948c7c654b901a172ae33
Mats Dahlström <Xats@gmail.com>
2013-02-26 15:25:37 +0100

    missed files?

--- LevelEditor test/RobotSpliteditor/LevelConstructor.h
+++ LevelEditor test/RobotSpliteditor/LevelConstructor.h
@@ -10,15 +10,16 @@ public:
 	LevelConstructor(const Level&);
 	LevelConstructor(const string&);
 	~LevelConstructor(void);
-	Level		getLevel();
-	void		addUnit(Unit*);
-//	void		addLaser(sf::Vector2f Position,std::string id,std::string Sprite);
-	void		addPlayer(Player*);
-	UnitVector&	accessObjects();
-	void		deleteItem(Unit*);
-	void		deletePlayer();
-	bool&		ifPlayerExist();
-	void		setNewName(const string&);
+	Level			getLevel();
+	sf::Vector2f	getSize();
+	void			addUnit(Unit*);
+//	void			addLaser(sf::Vector2f Position,std::string id,std::string Sprite);
+	void			addPlayer(Player*);
+	UnitVector&		accessObjects();
+	void			deleteItem(Unit*);
+	void			deletePlayer();
+	bool&			ifPlayerExist();
+	void			setNewName(const string&);
 private:
 	bool		mPlayerExist;
 	void		createLines();
